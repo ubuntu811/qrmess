b3a5aa1884f9f88f0764d2c3509e9d9bbb568292
Mirjam Zufferey <mirjam.zufferey@swisssign.com>
2024-08-08 17:11:20 +0200

    Black

--- qrmess.py
+++ qrmess.py
@@ -5,7 +5,7 @@ import random
 from pprint import pprint
 from fpdf import FPDF
 
-filename="./stuff.txt"
+filename = "./stuff.txt"
 
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
@@ -15,21 +15,21 @@ random.shuffle(lines)
 pdf = FPDF()
 pdf.add_page(orientation="Landscape", format="A4")
 
-nr=row=col=0
+nr = row = col = 0
 max_cols = 4
-col_width=40
-row_height=40
+col_width = 40
+row_height = 40
 
 for line in lines:
-  nr+=1
-  col += 1
-  if col > max_cols:
-    col = 1
-    row += 1
+    nr += 1
+    col += 1
+    if col > max_cols:
+        col = 1
+        row += 1
 
-  img = qrcode.make(line)
+    img = qrcode.make(line)
 
-  img.save(f"img/{nr}.png")
-  pdf.image(f"img/{nr}.png", 10 + (col * col_width), 10 + (row * row_height), 30, 30)
+    img.save(f"img/{nr}.png")
+    pdf.image(f"img/{nr}.png", 10 + (col * col_width), 10 + (row * row_height), 30, 30)
 
 pdf.output("doc.pdf")
