0ec985af12a0f22137ac3fb019a89a22eaa394ef
Mirjam Zufferey <mirjam.zufferey@swisssign.com>
2024-08-28 08:45:23 +0200

    Read from stdin

--- qrmess.py
+++ qrmess.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
+import sys
 import qrcode
 import random
 from pprint import pprint
 from fpdf import FPDF
 
-filename = "./stuff.txt"
+if not sys.stdin.isatty():
+    lines = sys.stdin.readlines()
 
-with open(filename) as file:
-    lines = [line.rstrip() for line in file]
+else:
+    filename = "./stuff.txt"
+
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
 
 random.shuffle(lines)
 
@@ -16,7 +21,7 @@ pdf = FPDF()
 pdf.add_page(orientation="Landscape", format="A4")
 
 nr = row = col = 0
-max_cols = 4
+max_cols = 5
 col_width = 40
 row_height = 40
 
@@ -32,4 +37,5 @@ for line in lines:
     img.save(f"img/{nr}.png")
     pdf.image(f"img/{nr}.png", 10 + (col * col_width), 10 + (row * row_height), 30, 30)
 
+print("writing ./doc.pdf")
 pdf.output("doc.pdf")
